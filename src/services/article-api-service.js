b657324daf6f73b700d07ea349bb2c1a547460bb
Timothy Hanke <tim.hanke@gmail.com>
2021-01-13 17:51:17 -0500

    pointed auth and getArticles at API

--- src/services/article-api-service.js
+++ src/services/article-api-service.js
@@ -4,29 +4,40 @@ import STORE from "./dummy-store";
 
 const ArticleApiService = {
   async getArticles() {
-    return STORE.articles;
+    // return STORE.articles;
     // API version
-    // TODO: change /things to /articles
-    // const res = await fetch(`${config.API_ENDPOINT}/things`, {
-    //   headers: {},
-    // });
-    // return await (!res.ok
-    //   ? res.json().then((e) => Promise.reject(e))
-    //   : res.json());
+    const res = await fetch(`${config.API_ENDPOINT}/articles`, {
+      headers: {
+        Authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    });
+    return await (!res.ok
+      ? res.json().then((e) => Promise.reject(e))
+      : res.json());
   },
   async postArticle(articleUrl) {
     // a mock article to insert into STORE to test the front end
     // eventually the back-end will fill in the details when
     // given an URL
-    let newArticle = {
-      id: STORE.articles.length + 1,
-      url: articleUrl,
-      title: "This Is Not Really The Title",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosq.",
-      image: `https://loremflickr.com/750/300/landscape?random=${STORE.articles.length}`,
-    };
-    STORE.articles.push(newArticle);
+    // let newArticle = {
+    //   id: STORE.articles.length + 1,
+    //   url: articleUrl,
+    //   title: "This Is Not Really The Title",
+    //   description:
+    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosq.",
+    //   image: `https://loremflickr.com/750/300/landscape?random=${STORE.articles.length}`,
+    // };
+    // STORE.articles.push(newArticle);
+    const res = await fetch(`${config.API_ENDPOINT}/articles`, {
+      headers: {
+        Authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+      method: "POST",
+      body: JSON.stringify({ target_url: articleUrl }),
+    });
+    return await (!res.ok
+      ? res.json().then((e) => Promise.reject(e))
+      : res.json());
   },
   async getArticle(articleId) {
     const res = await fetch(`${config.API_ENDPOINT}/articles/${articleId}`, {
--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -1,46 +1,46 @@
 import config from "../config";
-import STORE from "./dummy-store";
+// import STORE from "./dummy-store";
 
 const AuthApiService = {
   async postLogin(credentials) {
-    const dbUser = STORE.users.find(
-      (u) => u.user_name === credentials.user_name
-    );
-    if (!dbUser) {
-      return { error: "Incorrect user_name or password" };
-    }
-    const passwordMatch = dbUser.password === credentials.password;
-    if (!passwordMatch) {
-      return { error: "Incorrect user_name or password" };
-    }
-    return { authToken: "my-fake-testing-token" };
+    // const dbUser = STORE.users.find(
+    //   (u) => u.user_name === credentials.user_name
+    // );
+    // if (!dbUser) {
+    //   return { error: "Incorrect user_name or password" };
+    // }
+    // const passwordMatch = dbUser.password === credentials.password;
+    // if (!passwordMatch) {
+    //   return { error: "Incorrect user_name or password" };
+    // }
+    // return { authToken: "my-fake-testing-token" };
     // API version
-    // const res = await fetch(`${config.API_ENDPOINT}/auth/login`, {
-    //   method: "POST",
-    //   headers: { "content-type": "application/json" },
-    //   body: JSON.stringify(credentials),
-    // });
-    // return await (!res.ok
-    //   ? res.json().then((e) => Promise.reject(e))
-    //   : res.json());
+    const res = await fetch(`${config.API_ENDPOINT}/auth/login`, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(credentials),
+    });
+    return await (!res.ok
+      ? res.json().then((e) => Promise.reject(e))
+      : res.json());
   },
   async postUser(user) {
     // very naive registration to get static client working
     // without any backend API
-    STORE.users.push(user);
-    console.log(`STORE.users: ${STORE.users}`);
-    return user;
+    // STORE.users.push(user);
+    // console.log(`STORE.users: ${STORE.users}`);
+    // return user;
     // API version
-    // const res = await fetch(`${config.API_ENDPOINT}/users`, {
-    //   method: "POST",
-    //   headers: {
-    //     "content-type": "application/json",
-    //   },
-    //   body: JSON.stringify(user),
-    // });
-    // return await (!res.ok
-    //   ? res.json().then((e) => Promise.reject(e))
-    //   : res.json());
+    const res = await fetch(`${config.API_ENDPOINT}/users`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    return await (!res.ok
+      ? res.json().then((e) => Promise.reject(e))
+      : res.json());
   },
 };
 
