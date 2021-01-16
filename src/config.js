27c113d881e09712f830f7a52231034eb6146ff0
Timothy Hanke <tim.hanke@gmail.com>
2021-01-15 22:28:35 -0500

    fixed API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  API_ENDPOINT: "https://blooming-bastion-71420.herokuapp.com/",
+  API_ENDPOINT: "https://blooming-bastion-71420.herokuapp.com",
   TOKEN_KEY: "marker-monkey-client-auth-token",
 };
