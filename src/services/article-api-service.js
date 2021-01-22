6b0ce3229fb5cb55e71de499f7a2a32b9705e37a
Timothy Hanke <tim.hanke@gmail.com>
2021-01-21 22:24:07 -0500

    added delete functionality

--- src/services/article-api-service.js
+++ src/services/article-api-service.js
@@ -1,11 +1,8 @@
 import TokenService from "./token-service";
 import { API_ENDPOINT } from "../config";
-// import STORE from "./dummy-store";
 
 const ArticleApiService = {
   async getArticles() {
-    // return STORE.articles;
-    // API version
     const res = await fetch(`${API_ENDPOINT}/articles`, {
       headers: {
         Authorization: `bearer ${TokenService.getAuthToken()}`,
@@ -16,18 +13,6 @@ const ArticleApiService = {
       : res.json());
   },
   async postArticle(articleUrl) {
-    // a mock article to insert into STORE to test the front end
-    // eventually the back-end will fill in the details when
-    // given an URL
-    // let newArticle = {
-    //   id: STORE.articles.length + 1,
-    //   url: articleUrl,
-    //   title: "This Is Not Really The Title",
-    //   description:
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosq.",
-    //   image: `https://loremflickr.com/750/300/landscape?random=${STORE.articles.length}`,
-    // };
-    // STORE.articles.push(newArticle);
     const res = await fetch(`${API_ENDPOINT}/articles/`, {
       headers: {
         Authorization: `bearer ${TokenService.getAuthToken()}`,
@@ -35,6 +20,19 @@ const ArticleApiService = {
       },
       method: "POST",
       body: JSON.stringify({ target_url: articleUrl }),
+    });
+    return await (!res.ok
+      ? res.json().then((e) => Promise.reject(e))
+      : res.json());
+  },
+  async deleteUserArticle(articleId) {
+    const res = await fetch(`${API_ENDPOINT}/user_articles`, {
+      headers: {
+        Authorization: `bearer ${TokenService.getAuthToken()}`,
+        "Content-Type": "application/json",
+      },
+      method: "DELETE",
+      body: JSON.stringify({ article_id: articleId }),
     });
     return await (!res.ok
       ? res.json().then((e) => Promise.reject(e))
